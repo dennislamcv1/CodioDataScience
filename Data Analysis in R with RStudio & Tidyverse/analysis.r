6db13b86ccafa0a3fe55139c935b821c5fb697a0
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2022-07-17 14:19:45 +0800

    Add files via upload

---
+++ Data Analysis in R with RStudio & Tidyverse/analysis.r
@@ -0,0 +1,12 @@
+# write your code below in RStudio
+# then press the Source button to run it
+
+library(tidyverse)
+
+# add your code below this line
+
+d <- read.csv("all-ages.csv")
+print(d)
+
+
+# add your code above this line
