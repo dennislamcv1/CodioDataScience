35934d8dd07bc24a11d408610decb69b0050af6f
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2026-02-24 06:23:25 +0800

    Add files via upload

---
+++ Data Analysis in Python with pandas/hello.py
@@ -0,0 +1,15 @@
+x = 20
+y = 50
+z = 40
+
+import statistics
+
+data=[x,y,z]
+
+print(statistics.mean(data))
+
+max_data = max(data)
+min_data = min(data)
+
+range_data = max_data - min_data
+range_data
